c91325f7fef93448eeafa7c2355b40bec0efc832
Chris Meyers <chris.meyers.fsu@gmail.com>
2015-09-17 21:34:52 -0400

    debug

--- test/integration/docker/adds/init.sh
+++ test/integration/docker/adds/init.sh
@@ -6,7 +6,8 @@ source ./hacking/env-setup
 cd /ansible/test/integration
 
 # TODO: role_synchronize hanging
-export TEST_FLAGS="-vvvvv --skip-tags \"test_synchronize\""
+#export TEST_FLAGS="-vvvvv --skip-tags \"test_synchronize\""
+export TEST_FLAGS="-vvvvv --tags \"test_copy\""
 export CREDENTIALS_FILE=""
 make non_destructive && make destructive
 
